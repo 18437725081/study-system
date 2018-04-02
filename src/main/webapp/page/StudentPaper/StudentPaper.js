03aaf68e7ccfaae21f0afa1880d4e56a19c14a2e
Cranky <272969888@qq.com>
2018-04-02 15:46:47 +0800

    学生答卷功能开发

--- src/main/webapp/page/StudentPaper/StudentPaper.js
+++ src/main/webapp/page/StudentPaper/StudentPaper.js
@@ -1,5 +1,6 @@
 var selectedTr = null;
 
+//获取未完成的试卷列表
 function getPaper() {
     selectedTr = null;
     $.ajax({
@@ -21,7 +22,40 @@ function getPaper() {
     });
 }
 
-
+//跳转答卷页面
 function exam(pkPaper) {
     window.location.href = "Examination.html?"+pkPaper;
+}
+
+//获取试卷详情
+function getPaperDetail(pkPaper) {
+    $.ajax({
+        url: "../../student/getPaperDetail.do",
+        type: 'post',
+        data:{
+            pkPaper:pkPaper
+        },
+        success: function (data) {
+            if (data.status === 10) {
+                window.parent.location.href = "../../login.html";
+            } else if (data.status === 1) {
+                swal("", data.msg, "error");
+            } else {
+                document.getElementById('content-header').innerHTML = template('template', data);
+            }
+        },
+        error: function () {
+            window.location.href = "../other/error500.html";
+        }
+    });
+}
+
+//提交试卷
+function submitPaper() {
+
+    var pkTests = $("input[name=pkTest]");
+    var answers = $("select[name=answer]");
+    var scores = $(".score");
+
+
 }
