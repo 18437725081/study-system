b7e0d54fe1dbe2b4c19e598f35a5da3a0413938c
Cranky <272969888@qq.com>
2018-10-19 16:39:40 +0800

    MD5加密类调整

--- src/main/java/com/bs/util/Md5Util.java
+++ src/main/java/com/bs/util/Md5Util.java
@@ -10,17 +10,38 @@ import java.security.MessageDigest;
  **/
 public class Md5Util {
 
-    private static String byteArrayToHexString(byte[] b) {
-        StringBuilder resultSb = new StringBuilder();
-        for (byte aB : b) {
-            resultSb.append(byteToHexString(aB));
+    public static String md5EncodeUtf8(String origin) {
+        origin = origin + "qchclhg(1/2)y(xd+j)@walwj";
+        return MD5Encode(origin,"utf-8");
+    }
+
+    public static String MD5Encode(String origin, String charsetname){
+        String resultString = null;
+        try{
+            resultString = new String(origin);
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            if(null == charsetname || "".equals(charsetname)){
+                resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
+            }else{
+                resultString = byteArrayToHexString(md.digest(resultString.getBytes(charsetname)));
+            }
+        }catch (Exception e){
+        }
+        return resultString;
+    }
+
+
+    public static String byteArrayToHexString(byte b[]){
+        StringBuffer resultSb = new StringBuffer();
+        for(int i = 0; i < b.length; i++){
+            resultSb.append(byteToHexString(b[i]));
         }
         return resultSb.toString();
     }
-    
-    private static String byteToHexString(byte b) {
+
+    public static String byteToHexString(byte b){
         int n = b;
-        if (n < 0){
+        if(n < 0){
             n += 256;
         }
         int d1 = n / 16;
@@ -28,25 +49,6 @@ public class Md5Util {
         return HEX_DIGITS[d1] + HEX_DIGITS[d2];
     }
 
-    /**
-     * 返回大写MD5
-     */
-    private static String md5Encode(String origin) {
-        String resultString = null;
-        try {
-            resultString = origin;
-            MessageDigest md = MessageDigest.getInstance("Md5Util");
-            resultString = byteArrayToHexString(md.digest(resultString.getBytes("utf-8")));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return resultString.toUpperCase();
-    }
-
-    public static String md5EncodeUtf8(String origin) {
-        origin = origin + "qchclhg,(1/2)y(xd+j)@walwj";
-        return md5Encode(origin);
-    }
 
     private static final String[] HEX_DIGITS = {"0", "1", "2", "3", "4", "5",
             "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
